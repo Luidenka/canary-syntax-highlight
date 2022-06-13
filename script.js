1b1d7c73c1a9422d749938bcda9b9c1fb1e99a21
Luidenka <jarvislui1128@gmail.com>
2022-06-14 07:43:25 +0800

    Syntax Highlighting for Keywords

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const canary = {
         text = text.replace(/(#.*)/gm, "<span class=\"highlight-comment\">$1</span>")
         .replace(/([0-9])/gm, "<em>$1</em>")
 
-        console.log(text);
+        var without_comments = text.replace(/#.*/gm, "");
+
+        text = without_comments.replace(/\b(and|as|assert|break|class|continue|def|del|elif|else|except|False|finally|for|from|global|if|import|in|is|lambda|None|nonlocal|notor|pass|raise|return|True|try|while|with|yield)\b/gm,
+        "<span class=\"highlight-kw\">$1</span>");
 
         el.innerHTML = text.replace(/\n/gm, "<br>");
     }
