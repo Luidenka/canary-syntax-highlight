11ebabc9c803330b720d924877e51516fccbf41e
Luidenka <jarvislui1128@gmail.com>
2022-06-14 07:18:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const canary = {
         text.replace(/(#.*)/gm, "<span class=\"highlight-comment\">$1</span>")
         .replace(/([0-9])/gm, "<em>$1</em>")
 
+        console.log(text);
+
         el.innerHTML = text.replace(/\n/gm, "<br>");
     }
 }
